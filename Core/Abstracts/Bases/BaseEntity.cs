1feb314e3371809c2c64a05cdf2636db165c5986
Muhammet Akalın <akalinmuhammmetyz@gmail.com>
2026-03-16 14:54:48 +0300

    Update 03 16.03.2026

---
+++ Core/Abstracts/Bases/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core.Abstracts
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }= DateTime.Now;
+        public bool Active { get; set; } = true;
+        public bool Deleted { get; set; } = false;
+    }
+}
